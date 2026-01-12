99012364b4d031f793054a7e08f51d3e0a3ca878
andrido <matheusandradeskr@gmail.com>
2026-01-12 02:40:57 -0300

    corrigido o data.sql

--- diversos back-end/src/main/resources/data.sql
+++ diversos back-end/src/main/resources/data.sql
@@ -2,10 +2,10 @@
 -- Vamos focar em corrigir o INSERT, que é o que falhou.
 
 -- Correção: Removida a coluna 'id' e seus valores fixos
-INSERT INTO usuarios (cpf, email, bairro, cep, cidade, complemento, estado, numero, rua, nome, senha, status, telefone, tipo_de_usuario, username) VALUES
-('00000000000','moderador@diversos.com','jardim guanabara','60346245','Fortaleza','Bloco 7B','Ceará','700','Rosa Leite de Oliveira','zezin da silva','$2a$10$0v24GTJt/zRGLlq0Q4Bgw.4.F4o4L00bVVJyr9Tgb3BdUiRQHGV4C',NULL,'8599999999','MODERADOR','zedelivery'),
-('00000000000','usuario@diversos.com','aldeota','60115000','Fortaleza','Apartamento 101','Ceará','250','Rua Osvaldo Cruz','Fernandinho do Forró Moral','$2a$10$Z8tujhS6dVQFjB7J0.Wep.d76tt3x1MkirDBhOfgpD6PJA.3z8U.a',NULL,'8599999999','USUARIO','andrido'),
-('99999999999','admin@diversos.com','centro','60000000','Fortaleza','','Ceará','100','Avenida Principal','testado dos santos','$2a$10$3OQHjbhjVW1f4QgEiV3eGOrQaAF6cFeSF1PeBGgFI9txxMBwVifXO',NULL,'8599999999','ADMINISTRADOR','teste1');
+INSERT INTO usuarios (cpf, email, bairro, cep, cidade, complemento, estado, numero, rua, nome, senha, status, telefone, tipo_de_usuario) VALUES
+('00000000000','moderador@diversos.com','jardim guanabara','60346245','Fortaleza','Bloco 7B','Ceará','700','Rosa Leite de Oliveira','zezin da silva','$2a$10$0v24GTJt/zRGLlq0Q4Bgw.4.F4o4L00bVVJyr9Tgb3BdUiRQHGV4C',NULL,'8599999999','MODERADOR'),
+('00000000000','usuario@diversos.com','aldeota','60115000','Fortaleza','Apartamento 101','Ceará','250','Rua Osvaldo Cruz','Fernandinho do Forró Moral','$2a$10$Z8tujhS6dVQFjB7J0.Wep.d76tt3x1MkirDBhOfgpD6PJA.3z8U.a',NULL,'8599999999','USUARIO'),
+('99999999999','admin@diversos.com','centro','60000000','Fortaleza','','Ceará','100','Avenida Principal','testado dos santos','$2a$10$3OQHjbhjVW1f4QgEiV3eGOrQaAF6cFeSF1PeBGgFI9txxMBwVifXO',NULL,'8599999999','ADMINISTRADOR');
 
 
 -- Correção: Removida a coluna 'id' e seu valor fixo
