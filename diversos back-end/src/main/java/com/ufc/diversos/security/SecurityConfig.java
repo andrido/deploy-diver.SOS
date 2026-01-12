5fcb00124c2d9395c144100f90292fbdb3a5ff9c
andrido <matheusandradeskr@gmail.com>
2026-01-11 23:19:09 -0300

    corrigida a permissão da origin do deploy

--- diversos back-end/src/main/java/com/ufc/diversos/security/SecurityConfig.java
+++ diversos back-end/src/main/java/com/ufc/diversos/security/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://diversos-frontend.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
